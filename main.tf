08d98e1d546687156feb00c94a4578b9a341e54c
veluguvijay <veluguvijay1998@gmail.com>
2025-10-31 15:39:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count                  = 4
-  ami                    = "ami-0360c520857e3138f"
+  ami                    = "ami-0bbdd8c17ed981ef9"
   instance_type          = "t2.medium"
-  key_name               = "Netflix"
+  key_name               = "bedroomkey"
   vpc_security_group_ids = ["sg-0ead21b7862e54f9a"]
   tags = {
     Name = var.instance_names[count.index]
